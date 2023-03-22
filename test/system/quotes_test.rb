af5da64987a4f43b5ec88e5fb0eea9a6b7304fe1
Enrique Gonzalez <enriikke@gmail.com>
2023-03-22 16:23:08 -0400

    Finish form creation/edit flow and get test passing
    Signed-off-by: Enrique Gonzalez <enriikke@gmail.com>

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,50 +1,50 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class QuotesTest < ApplicationSystemTestCase
-  fixtures do
+  setup do
     @quote = quotes(:first)
   end
 
-  test "creating a new quote" do
+  test 'creating a new quote' do
     visit quotes_path
-    assert_selector "h1", text: "Quotes"
+    assert_selector 'h1', text: 'Quotes'
 
-    click_on "New Quote"
-    assert_selector "h1", text: "New Quote"
+    click_on 'New quote'
+    assert_selector 'h1', text: 'New Quote'
 
-    fill_in "Name", with: "Test Quote"
-    click_on "Create Quote"
+    fill_in 'Name', with: 'Test Quote'
+    click_on 'Create quote'
 
-    assert_selector "h1", text: "Quotes"
-    assert_text "Test Quote"
+    assert_selector 'h1', text: 'Quotes'
+    assert_text 'Test Quote'
   end
 
-  test "showing a quote" do
+  test 'showing a quote' do
     visit quotes_path
     click_link @quote.name
 
-    assert_selector "h1", text: @quote.name
+    assert_selector 'h1', text: @quote.name
   end
 
-  test "updating a quote" do
+  test 'updating a quote' do
     visit quotes_path
-    assert_selector "h1", text: "Quotes"
+    assert_selector 'h1', text: 'Quotes'
 
-    click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit Quote"
+    click_on 'Edit', match: :first
+    assert_selector 'h1', text: 'Edit Quote'
 
-    fill_in "Name", with: "Updated Quote"
-    click_on "Update Quote"
+    fill_in 'Name', with: 'Updated Quote'
+    click_on 'Update quote'
 
-    assert_selector "h1", text: "Quotes"
-    assert_text "Updated Quote"
+    assert_selector 'h1', text: 'Quotes'
+    assert_text 'Updated Quote'
   end
 
-  test "destroying a quote" do
+  test 'destroying a quote' do
     visit quotes_path
     assert_text @quote.name
 
-    click_on "Delete", match: :first
+    click_on 'Delete', match: :first
     assert_no_text @quote.name
   end
 end
